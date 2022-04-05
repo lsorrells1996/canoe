be829c4b8a8722c977cefef88dc4eb3bd312f85c
Lorenzo Sorrells <lsorrells1996@gmail.com>
2022-04-05 14:12:05 -0500

    Configured client-side routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get '*path', to: 'fallback#index', constraints: ->(req) { !req.xhr? && req.format.html? }
   # Defines the root path route ("/")
   # root "articles#index"
 end
