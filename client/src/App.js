8b4da66d097cc02d6c8cb526f43bb356cb9965a7
Lorenzo Sorrells <lsorrells1996@gmail.com>
2022-04-07 11:13:20 -0500

    added basic navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import Login from "./Components/Login";
+import NavBar from "./Components/NavBar";
 import Signup from "./Components/Signup";
 
 function App() {
@@ -9,11 +10,14 @@ function App() {
   const [user, setUser] = useState(null);
 
   return (
-    <Routes>
-      <Route path="/" element={ <Login setUser={ setUser } /> } />
-      <Route path="/signup" element={ <Signup setUser={ setUser } /> } />
-      <Route path="/home" element={ <Home setUser={ setUser } /> } />
-    </Routes>
+    <>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={ <Login setUser={ setUser } /> } />
+        <Route path="/signup" element={ <Signup setUser={ setUser } /> } />
+        <Route path="/home" element={ <Home setUser={ setUser } /> } />
+      </Routes>
+    </>
   );
   }
 
